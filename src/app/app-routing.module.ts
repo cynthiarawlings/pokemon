b6f426b19d16521a6f2150c03affdc523e05b6bc
hyruleismyrule <hyruleismyrule@gmail.com>
2023-03-27 19:57:23 -0400

    added bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { SetResolverService } from './set-resolver.service';
+import { SetsComponent } from './sets/sets.component';
+import { SetStartComponent } from './sets/set-start/set-start.component';
+import { SetEditComponent } from './sets/set-edit/set-edit.component';
+import { SetDetailComponent } from './sets/set-detail/set-detail.component';
+
 const routes: Routes = [
-  // { path: "", redirectTo: "/sets", pathMatch: "full" },
-  //   { path: "sets", component: SetsComponent, children: [
-  //       { path: "", component: SetStartComponent },
-  //       { path: "new", component: SetEditComponent },
-  //       // { path: ":id", component: SetDetailComponent, resolve: [RecipeResolverService] },
-  //       // { path: ":id/edit", component: SetEditComponent, resolve: [RecipeResolverService] }
-  //   ] }
-    // { path: "shopping-list", component: ShoppingListComponent }
+  { path: "", redirectTo: "/sets", pathMatch: "full" },
+    { path: "sets", component: SetsComponent, children: [
+        { path: "", component: SetStartComponent },
+        { path: "new", component: SetEditComponent },
+        { path: ":id", component: SetDetailComponent, resolve: [SetResolverService] },
+        { path: ":id/edit", component: SetEditComponent, resolve: [SetResolverService] }
+    ] }
 ];
 
 @NgModule({
